0bcef19e8b320864e628c9b10546739663fe29d7
Rob Koch <Rob@macbook-pro-6.local>
2016-10-10 18:16:07 -0400

    Initial Commit

---
+++ Project Dragonfly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project Dragonfly
+//
+//  Created by Rob Koch on 10/10/16.
+//  Copyright © 2016 cse.miamioh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
