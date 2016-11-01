c26b91e20470aaf8498804b9b2f6852771997b34
zeldemire <zeldemire@gmail.com>
2016-11-01 09:35:53 -0400

    Added variables in Ivestigation file

--- Project Dragonfly/Models/Investigation.swift
+++ Project Dragonfly/Models/Investigation.swift
@@ -4,3 +4,33 @@
 //
 
 import Foundation
+
+class Investigation {
+    var question: String
+    var components: [Component]
+    var date: Date
+    var timer: Foundation.NSTimer?
+
+    init(question: String, components: [Component]) {
+        self.components = components
+        self.question = question
+        self.date = Date()
+    }
+
+    var lastUpdated: String {
+        get {
+            let format: String
+            let now = Date().timeIntervalSince1970 * 1000
+            switch now - date.timeIntervalSince1970 * 1000 {
+            case 0 ..< 72000000: format = "h:mm a"
+            case 72000000 ..< 432000000: format = "EEEE"
+            case 432000000 ..< 12000000000: format = "M/d"
+            default: format = "MMM 'yy"
+            }
+            let formatter = DateFormatter()
+            formatter.dateFormat = format
+            return formatter.string(from: date)
+        }
+    }
+
+}
