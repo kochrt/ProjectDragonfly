a2beeb1a5b0af1178962442e9463185a76106b72
Rob Koch <Rob@MacBook-Pro-7.local>
2016-11-07 19:56:51 -0500

    categories and strings and more

--- Category.swift
+++ Category.swift
@@ -5,18 +5,47 @@
 
 import Foundation
 
-class Category {
+class Category: Comparable {
+    
+    var title: String
     var investigations: [Investigation]
     
-    init() {
+    var count: Int {
+        get {
+            return investigations.count
+        }
+    }
+    
+    init(title: String) {
         self.investigations = [Investigation]()
+        self.title = title
     }
     
     func add(investigation: Investigation) {
         self.investigations.append(investigation)
     }
     
-    func getSize() -> Int {
-        return investigations.count
+    /// Returns a Boolean value indicating whether the value of the first
+    /// argument is less than that of the second argument.
+    ///
+    /// This function is the only requirement of the `Comparable` protocol. The
+    /// remainder of the relational operator functions are implemented by the
+    /// standard library for any type that conforms to `Comparable`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func <(lhs: Category, rhs: Category) -> Bool {
+        return lhs.title < rhs.title
     }
+    
+    public static func ==(lhs: Category, rhs: Category) -> Bool {
+        return lhs.title == rhs.title
+    }
+    
+}
+
+class CategoryList {
+    static let instance = CategoryList()
+    var list = [Category]()
 }
