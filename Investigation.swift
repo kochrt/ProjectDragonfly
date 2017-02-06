9a2f83e081515186e4210c98734088d7d279e35e
Marian Willard <marianwillard@yahoo.com>
2017-02-06 17:17:49 -0500

    saving works of timerLength

--- Investigation.swift
+++ Investigation.swift
@@ -45,7 +45,7 @@ class Investigation: NSObject, NSCoding {
         self.date = decoder.decodeObject(forKey: Keys.date) as! Date
         self.title = decoder.decodeObject(forKey: Keys.title) as! String
         self.category = decoder.decodeObject(forKey: Keys.category) as! String
-        self.timerLength = decoder.decodeObject(forKey: Keys.timerLength) as? Double ?? 0.0
+        self.timerLength = decoder.decodeDouble(forKey: Keys.timerLength)
         self.componentType = ComponentEnum(rawValue: decoder.decodeObject(forKey: Keys.componentType) as! String)!
     }
     
