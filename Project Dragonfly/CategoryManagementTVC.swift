f2511d6a20d5917874abe37e6cfa3b7ae99dfa20
Marian Willard <marianwillard@yahoo.com>
2017-03-30 19:16:21 -0400

    stuff

--- Project Dragonfly/CategoryManagementTVC.swift
+++ Project Dragonfly/CategoryManagementTVC.swift
@@ -133,4 +133,6 @@ class CategoryManagementTVC: CategoriesTVC {
         }
     }
     
+   
+    
 }
--- Project Dragonfly/ChooseCategoryVC.swift
+++ Project Dragonfly/ChooseCategoryVC.swift
@@ -27,5 +27,12 @@ class ChooseCategoryVC: CategoriesTVC {
         
         dismiss(animated: true, completion: nil)
     }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.delegate?.backFromCamera()
+//    }
+    
+    
 
 }
