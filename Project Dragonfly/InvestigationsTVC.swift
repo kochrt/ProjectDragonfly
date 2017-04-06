76ae5f3f434f2e914294e7bd773d0e66c006ada6
Marian Willard <marianwillard@yahoo.com>
2017-04-06 16:10:45 -0400

    temporary hello alert in InvestigationsTVC

--- Project Dragonfly/InvestigationsTVC.swift
+++ Project Dragonfly/InvestigationsTVC.swift
@@ -16,7 +16,10 @@ class InvestigationsTVC: UITableViewController, NewInvestigationDelegate, DZNEmp
         static let CreateInvestigation = "createInvestigation"
     }
     
+    let infoAlert = UIAlertController(title: "Hello!", message: "Welcome to the Dragonfly app! This app is used to investigate your environment.", preferredStyle: .alert)
+    
     @IBAction func help(_ sender: Any) {
+        self.present(infoAlert, animated: true, completion: nil)
     }
 
     
@@ -27,6 +30,8 @@ class InvestigationsTVC: UITableViewController, NewInvestigationDelegate, DZNEmp
         self.tableView.emptyDataSetDelegate = self
         
         self.tableView.tableFooterView = UIView()
+        setupInfoAlert()
+        self.present(infoAlert, animated: true, completion: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -148,6 +153,12 @@ class InvestigationsTVC: UITableViewController, NewInvestigationDelegate, DZNEmp
     
     func verticalOffset(forEmptyDataSet scrollView: UIScrollView!) -> CGFloat {
         return -20.0
+    }
+    
+    // MARK: Alert setup
+    func setupInfoAlert() {
+        infoAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in }))
+        
     }
 }
 
