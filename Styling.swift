aba9687261329357adf7777911239023d7796f77
Laufenberg <laufengd@CEC-CAPSTONE06.local>
2017-04-10 16:32:00 -0400

    add chart colors to style

--- Styling.swift
+++ Styling.swift
@@ -19,7 +19,21 @@ struct Styling {
     struct Colors {
         static let Primary = UIColor(colorLiteralRed: 51/255.0, green: 133/255.0, blue: 204/255.0, alpha: 1.0)
         static let Secondary = UIColor(colorLiteralRed: 24/255.0, green: 175/255.01, blue: 140/255.0, alpha: 1.0)
+        
+        var resultColors: [UIColor] = [
+            UIColor(red: 24, green: 175, blue: 140, alpha: 1),
+            UIColor(red: 44, green: 58, blue: 68, alpha:1),
+            UIColor(red: 231, green: 110, blue: 114, alpha: 1),
+            UIColor(red: 172, green: 189, blue: 201, alpha: 1),
+            UIColor(red: 51, green: 133, blue: 204, alpha: 1),
+            UIColor(red: 240, green: 133, blue: 71, alpha: 1),
+            UIColor(red: 149, green: 149, blue: 149, alpha: 1),
+            UIColor(red: 66, green: 88, blue: 102, alpha: 1),
+            UIColor(red: 153, green: 76, blue: 0, alpha: 1),
+            UIColor(red: 150, green: 97, blue: 219, alpha: 1)]
     }
+    
+
     
     static let HeaderFont = UIFont(name: Values.FontName, size: Values.HeaderSize)!
     
