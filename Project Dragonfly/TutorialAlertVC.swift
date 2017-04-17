26e79ca89008040ee2f6028d534be51c13521b39
Rob Koch <kochrt@gmail.com>
2017-04-17 16:41:08 -0400

    handler for TutorialAVC

--- Project Dragonfly/TutorialAlertVC.swift
+++ Project Dragonfly/TutorialAlertVC.swift
@@ -13,7 +13,7 @@ class TutorialAlertVC {
     static func create(title: String, messages: [String], buttonTitle: String = "OK", handler: @escaping () -> Void = {}) -> UIAlertController {
         
         let paragraphStyle = NSMutableParagraphStyle()
-    	let messageText = NSMutableAttributedString(
+        let messageText = NSMutableAttributedString(
             string: "\nWelcome to the Dragonfly app!\n\n This app is used to investigate your environment. \n\n Simply create an investigation to get started (click the plus in the upper right corner). ",
             attributes: [
                 NSParagraphStyleAttributeName: paragraphStyle,
@@ -22,22 +22,9 @@ class TutorialAlertVC {
             ])
         let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
         alert.setValue(messageText, forKey: "attributedMessage")
-        alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: handler))
+        alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: { (_) in
+            handler()
+        }))
+        return alert
     }
-    
-//    init(title: String, messages: [String], buttonText: String) {
-//        let paragraphStyle = NSMutableParagraphStyle()
-//        let messageText = NSMutableAttributedString(
-//            string: "\nWelcome to the Dragonfly app!\n\n This app is used to investigate your environment. \n\n Simply create an investigation to get started (click the plus in the upper right corner). ",
-//            attributes: [
-//                NSParagraphStyleAttributeName: paragraphStyle,
-//                NSFontAttributeName : UIFont.preferredFont(forTextStyle: UIFontTextStyle.body),
-//                NSForegroundColorAttributeName : UIColor.black
-//            ])
-//        setValue(messageText, forKey: "attributedMessage")
-//        addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in }))
-    
-//        super.init(title: title, message: "", preferredStyle: .alert)
-//    }
-
 }
