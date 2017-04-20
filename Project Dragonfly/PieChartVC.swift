b59748a0d9ed1116ad4a17c87673de6548663753
zeldemire <zeldemire@gmail.com>
2017-04-20 17:02:17 -0400

    Added comments

--- Project Dragonfly/PieChartVC.swift
+++ Project Dragonfly/PieChartVC.swift
@@ -11,62 +11,78 @@ import Charts
 
 class PieChartVC: ChartVC, ChartViewDelegate {
 
+    // Variable for holding all of the data for the slices on the graph.
     var pieDataEntries = [ChartDataEntry]()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Variable for setting the chartView as a PieChartView.
+        // This is needed to be able to display the pie chart on the page.
         let pieChart = chartView as! PieChartView
+
+        // Call the pieChart func to display the data.
         pieChartEnable(pieChart: pieChart)
-        // Do any additional setup after loading the view.
     }
 
     func pieChartEnable(pieChart: PieChartView) {
+        // Make the pieChart view be able to receive events.
         pieChart.delegate = self
+
+        // Sets the title of the graph to the investigation question.
         chartTitle.text = investigation.question
-        
-        
-        var i = 0
-        for values in investigation.getInfo() {
+
+        // Sets the inner text of the pieChart to display the seconds of the of the IntervalCounter.
+        if investigation.componentType == .IntervalCounter {
+            pieChart.centerText = String(investigation.getTime()) + " seconds"
+        }
+
+        // Get the information from the investigation ie count amount and name of the item.
+        let info = investigation.getInfo()
+
+        // Loop through the values and it to the pieDataEntries object.
+        for (var i, values) in info.enumerated() {
             let dataEntry = PieChartDataEntry(value: Double(values.value), label: values.name, data: Double(i) as AnyObject?)
             pieDataEntries.append(dataEntry)
             i += 1
         }
-        
+
+        // Variable to hold the slices for the pie chart.
         let pieDataSet = PieChartDataSet(values: pieDataEntries, label: "")
-        pieChart.chartDescription?.text = ""
+
+        // The space in pixels between the pie-slices
         pieDataSet.sliceSpace = 4.0
-        
+
+        // Set the color of slices to the colors defined in ChartVC.
         pieDataSet.colors = colors
-        
+
+        // Create a variable to hold the Pie Chart.
         let data = PieChartData(dataSet: pieDataSet)
-        
+
+        // Setting the font and color of the text.
         data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 13.0))
         data.setValueTextColor(UIColor.white)
+
+        // Remove labels from the PieChart slices.
         pieChart.drawEntryLabelsEnabled = false
-        
+
+        // Set the PieCharts data to the data from the investigation
         pieChart.data = data
+
+        // Create an animation for the PieCHart.
         pieChart.animate(yAxisDuration: 1.4, easingOption: ChartEasingOption.easeInCubic)
-        
-        if investigation.componentType == .IntervalCounter {
-            pieChart.centerText = String(investigation.getTime()) + " seconds"
-        } else if investigation.componentType == .Counter {
-            chartTitle.text = investigation.question
-        } else if investigation.componentType == .Stopwatch {
-            pieChart.centerText = String(investigation.getTime()) + " seconds"
-        }
-        
     }
-    
+
     @IBAction func share(_ sender: Any) {
         let shareString = "Check out this investigation I made in the Dragonfly App!"
-        
+
         let shareController = UIActivityViewController(activityItems: [shareString, getScreenshot()], applicationActivities: nil)
         if let popover = shareController.popoverPresentationController {
             popover.barButtonItem = sender as? UIBarButtonItem
             present(shareController, animated: true, completion: nil)
         }
     }
-    
-    
+
+
 }
+
